895637a43fe14426520484371152b3b67f91b85f
Pravin Zade <skyrites@gmail.com>
2020-05-19 13:02:48 +0530

    Styled Components & Dynamic Styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
+
 import Person from './Person/Person';
+
+const StyleButton = styled.button`
+  background-color: ${(props) => (props.alt ? 'red' : 'green')};
+  color: white;
+  font: inherit;
+  border: 2px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: ${(props) => (props.alt ? 'salmon' : 'lightgreen')};
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -79,11 +95,11 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black',
-      };
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black',
+      // };
     }
 
     let classes = [];
@@ -95,19 +111,20 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className='App'>
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button style={style} onClick={this.togglePersonsHandler}>
-            Toggle Persons
-          </button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className='App'>
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <StyleButton
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
+          Toggle Persons
+        </StyleButton>
+        {persons}
+      </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import Radium from 'radium';
-import './Person.css';
+import styled from 'styled-components';
+// import './Person.css';
+const StyleDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`;
 
 const person = (props) => {
   const style = {
@@ -9,14 +21,15 @@ const person = (props) => {
     },
   };
   return (
-    <div className='Person' style={style}>
+    // <div className='Person' style={style}>
+    <StyleDiv>
       <p onClick={props.click}>
         I'm a {props.name}. I am years {props.age} old!
       </p>
       <p>{props.children}</p>
       <input type='text' onChange={props.changed} value={props.name} />
-    </div>
+    </StyleDiv>
   );
 };
 
-export default Radium(person);
+export default person;
