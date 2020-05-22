bf0e1c8af22d3d9f47dc421af13740d69dd910b4
Pravin Zade <skyrites@gmail.com>
2020-05-22 16:32:42 +0530

    life cycle hooks detail

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Person.css';
 
-const person = (props) => {
-  console.log('[Person.js] rendering...');
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering...');
 
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>
-        I'm a {props.name}. I am years {props.age} old!
-      </p>
-      <p>{props.children}</p>
-      <input type='text' onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>
+          I'm a {this.props.name}. I am years {this.props.age} old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type='text'
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </div>
+    );
+  }
+}
 
-export default person;
+export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -24,12 +24,19 @@ class App extends Component {
     return state;
   }
 
-  componentWillMount() {
-    console.log('[App.js] componentWillMount');
-  }
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount');
+  // }
 
   componentDidMount() {
     console.log('[App.js] componentDidMount');
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
   }
 
   nameChangedHandler = (event, id) => {
