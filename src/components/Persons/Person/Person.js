954ea2f3bbdb89639aeb9589cff4ecd8dc7cfc79
Pravin Zade <skyrites@gmail.com>
2020-05-28 09:49:23 +0530

    Passing Unknown Props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
+import withClass from '../../../hoc/withClass';
 
 import classes from './Person.css';
 
@@ -24,4 +25,4 @@ class Person extends Component {
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,9 @@ import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxiliary';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -89,7 +92,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <Aux>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -106,10 +109,10 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </div>
+      </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
